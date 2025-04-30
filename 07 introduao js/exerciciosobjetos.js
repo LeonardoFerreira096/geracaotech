2eba004d5f187aa0be63fbe3028ec6dac56fff00
Leonardo Costa Ferreira <leonardo_costaferreira22@hotmail.com>
2025-04-30 16:44:50 -0300

    aula de objetos em JS

---
+++ 07 introduao js/exerciciosobjetos.js
@@ -0,0 +1,87 @@
+// 1.Crie um objeto chamado livro com as propriedades: titulo, autor, ano. 
+// Acesse e imprima o autor do livro. Modifique o ano do livro para 2023. 
+// Adicione uma nova propriedade editora.
+
+// const livro = {
+
+//     titulo: 'Homem Aranha',
+//     autor: 'Stan Lee',
+//     ano: '1967'
+
+// }
+
+// livro.ano = 2023
+// console.log(livro.ano)
+
+// 2.Dado o array abaixo, crie um laço que percorre os objetos 
+// e imprime o nome de cada aluno:
+
+
+// const alunos = [
+//     {
+//         nome: "João",
+//         nota: 8
+//     },
+
+//     {
+//         nome: "Maria",
+//         nota: 9
+
+//     },
+//     {
+//         nome: "Pedro",
+//         nota: 7
+
+//     }
+
+// ]
+
+// for (let i = 0; i < alunos.length; i++) {
+//     console.log(alunos[i].nome)
+// }
+
+// 3.Crie uma função que recebe um array de objetos de produtos 
+// e retorna a soma total dos preços:
+
+
+
+// const produtos = [
+//     {
+//         nome: "Camiseta",
+//         preco: 30
+//     },
+//     {
+//         nome: "Calça",
+//         preco: 80
+//     },
+//     {
+//         nome: "Boné",
+//         preco: 20
+
+//     }
+// ]
+
+// function totalPreco (array) {
+//     let soma = 0
+//     for (i = 0)
+// }   nao tá completa
+
+
+
+
+// 4.Crie uma função que recebe um array de produtos e o nome de um produto vendido.
+//  A função deve reduzir o estoque desse produto em 1.
+
+// const produtos = [
+//     { nome: "Camiseta", preco: 30 },
+//     { nome: "Calça", preco: 80 },
+//     { nome: "Boné", preco: 20 }
+//     ];
+
+//     function reduzirEstoque (array, itemVendido) {
+//         for (let i = 0; i < array.length; i++)
+//     } nao tá completa
+
+// 5.Crie uma função que recebe um array de alunos com nome e nota e retorna 
+// um novo array apenas com os alunos que tiveram nota maior ou igual a 7. (utilize o filter( ))
+
---
+++ 07 introduao js/objetos.js
@@ -0,0 +1,44 @@
+const alunos = ['Leonardo', 'Inessa', 'Carlos André']
+
+const pessoa = {
+    nome: 'Leonardo',
+    idade: 28,
+    genero: 'Masculino'
+}
+
+const produtos = [
+    {
+        id: 1,
+        img: 'Linkdaimagem',
+        nomeProd: 'nomedoproduto',
+        categoria: 'categoriadoproduto',
+        desconto: 'true',
+        preco: 'valordoproduto'
+    },
+    {
+        id: 2,
+        img: 'Linkdaimagem',
+        nomeProd: 'nomedoproduto',
+        categoria: 'categoriadoproduto',
+        desconto: 'true',
+        preco: 'valordoproduto'
+    },
+    {
+        id: 3,
+        img: 'Linkdaimagem',
+        nomeProd: 'nomedoproduto',
+        categoria: 'categoriadoproduto',
+        desconto: 'true',
+        preco: 'valordoproduto'
+    },
+    {
+        id: 4,
+        img: 'Linkdaimagem',
+        nomeProd: 'nomedoproduto',
+        categoria: 'categoriadoproduto',
+        desconto: 'true',
+        preco: 'valordoproduto'
+    }
+]
+
+console.log(produtos[0].id)
